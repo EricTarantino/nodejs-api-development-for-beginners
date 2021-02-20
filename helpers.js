3c350d46843f3df2210a3ce28c6b48e44a296124
Hamidi <eric.vanlessen@googlemail.com>
2021-02-20 22:12:21 +0100

    initial

---
+++ helpers.js
@@ -0,0 +1,11 @@
+console.log("hello from node.js");
+
+//function sum(a,b,){
+//    return a+b;
+//}
+
+// const sum = (a,b) => a + b;
+
+exports.sum = (a,b) => a + b;
+
+// console.log("Process", process);
