f277f97fe3a09155e76d0090f863d2e9e712dc68
EricTarantino <eric.vanlessen@live.de>
2021-02-21 08:54:05 +0100

    event loop

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const fileName = 'target.txt';
 
-fs.watch(fileName, (err, data) => {
+fs.readFile(fileName, (err, data) => {
     if (err){
         console.log(err);
     } else {
