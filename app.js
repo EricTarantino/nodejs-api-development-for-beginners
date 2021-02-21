339ad4ea65398041a420fbea0c033e6673773e48
EricTarantino <eric.vanlessen@live.de>
2021-02-21 12:21:23 +0100

    synchronous programming

--- app.js
+++ app.js
@@ -8,11 +8,8 @@ const errHandler = (err) => console.log(err);
 const dataHandler = (data) => console.log(data.toString());
 
 fs.readFile(fileName, (err, data) => {
-  if (err) {
-    errHandler(err);
-  } else {
-    dataHandler(data);
-  }
+  if (err) errHandler(err);
+  dataHandler(data);
 });
 
 console.log('Nodejs async programming');
