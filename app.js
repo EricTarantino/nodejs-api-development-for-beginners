97131a0003e4c35dbddd1620434f4583fc9b26b0
EricTarantino <eric.vanlessen@live.de>
2021-02-21 13:27:25 +0100

    middleware

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const app = express();
 
+const morgan = require('morgan');
+
+const myOwnMiddleware = (req, res, next) => {
+  console.log('Middleware applied!');
+  next();
+};
+
+app.use(myOwnMiddleware);
+
 // bring in routes
 const { getPosts } = require('./routes/post');
+
+// middleware
+app.use(morgan('dev'));
 
 app.get('/', getPosts);
 
