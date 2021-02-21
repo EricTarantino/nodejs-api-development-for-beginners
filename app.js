fcbf8c411420bb7a5c93c8fff5fa430e599aef6b
EricTarantino <eric.vanlessen@live.de>
2021-02-21 13:12:47 +0100

    express

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+const postRoutes = require('routes/post');
+
 app.get('/', (req, res) => {
   res.send('hello world from express');
 });
