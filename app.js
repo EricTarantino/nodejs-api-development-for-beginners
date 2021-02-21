eb441a34737f8f1623ffaace4fced573b5f9be75
EricTarantino <eric.vanlessen@live.de>
2021-02-21 08:53:32 +0100

    event loop

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const fs = require('fs');
+
 const fileName = 'target.txt';
 
-fs.watch(fileName, () => console.log(`File changed!`));
+fs.watch(fileName, (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        console.log(data.toString());
+    }
+});
 
+console.log("Nodejs async programming");
